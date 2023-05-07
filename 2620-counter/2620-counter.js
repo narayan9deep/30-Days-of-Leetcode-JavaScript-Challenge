74af5419a161b997234c69858e469a6f4633d976
NARAYAN <narayan9deep@gmail.com>
2023-05-07 19:17:33 +0530

    Day 2 solution

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let i=-1;
+    
+    return function() {
+        i++;
+        return n+i;
+        
+        
+    };
+ 
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
